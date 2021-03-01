a8c27bf85e4b10d749c9587f3a603177f6198cc1
emekarr <aceanyanwu02@gmail.com>
2021-03-02 00:06:30 +0100

    compelted the login of the server

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/user.js");
+const bcrypt = require("bcrypt");
 
 // this function is called to create a user in the database
 const create_user = async (req, res) => {
@@ -15,9 +16,32 @@ const create_user = async (req, res) => {
   if (!user) return res.send({ userCreated: false });
 
   const user_token = await user.generateToken();
-  res.send({ userCreated: true, token: user_token });
+  res.send({ user_created: true, token: user_token });
+};
+
+// this function is called to log a user into his account
+const login_user = async (req, res) => {
+  // request body data
+  const user_data = req.body;
+
+  try {
+    const searched_user_result = await User.findOne({ email: user_data.email });
+
+    if (!searched_user_result) throw new Error("Unregistered email address");
+
+    const password_check_result = await bcrypt.compare(
+      user_data.password,
+      searched_user_result.password
+    );
+
+    if (!password_check_result) throw new Error("Invalide login");
+    res.send({ valid_credentials: true });
+  } catch (e) {
+    res.send({ valid_credentials: false });
+  }
 };
 
 module.exports = {
   create_user,
+  login_user,
 };
--- app/routes/userRoute.js
+++ app/routes/userRoute.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {create_user} = require("../controllers/userController.js")
+const {create_user, login_user} = require("../controllers/userController.js")
 
 // create a new user in the database
 router.post("/signup", create_user);
+
+// log in an existing user
+router.post("/login", login_user)
 
 module.exports = router;
