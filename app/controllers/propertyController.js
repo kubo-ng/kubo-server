738f1992b5fa01aed40ee5bcddc147f28ee44454
emekarr <aceanyanwu02@gmail.com>
2021-03-14 11:13:23 +0100

    set up the server to delete properties when provided an id

--- app/controllers/propertyController.js
+++ app/controllers/propertyController.js
@@ -2,7 +2,7 @@ const Property = require("../models/property.js");
 
 const create_property = async (req, res) => {
   const property_info = req.body.property_info;
-  property_info.owner = req.body.user._id
+  property_info.owner = req.body.user._id;
 
   try {
     const property = new Property(property_info);
@@ -10,7 +10,7 @@ const create_property = async (req, res) => {
     if (!property) throw new Error("Failed to create new property.");
 
     await property.save();
-    res.send({ propertyCreated: true });
+    res.send({ propertyCreated: true, id: property._id });
   } catch (e) {
     res.send({
       propertyCreated: false,
@@ -25,13 +25,34 @@ const get_property_by_id = async (req, res) => {
   try {
     const property = await Property.findById(property_id);
     if (!property) throw new Error("Could not find user.");
-    res.send({property});
+    res.send({ property });
   } catch (e) {
     res.send({ property: null, error: "Failed to get user with id provided." });
+  }
+};
+
+const delete_property_id = async (req, res) => {
+  const property_id = req.query.id;
+  let is_property_deleted;
+
+  try {
+    const property = await Property.findByIdAndDelete(property_id);
+    if (!property)
+      throw new Error("Could not delete property with id provided.");
+
+    is_property_deleted = true;
+    res.send({ is_property_deleted });
+  } catch (e) {
+    is_property_deleted = false;
+    res.send({
+      is_property_deleted,
+      error: "Could not delete property with id provided.",
+    });
   }
 };
 
 module.exports = {
   create_property,
   get_property_by_id,
+  delete_property_id,
 };
--- app/routes/propertyRoute.js
+++ app/routes/propertyRoute.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const authMiddleware = require("../middlewares/authMiddleware.js");
-const { create_property, get_property_by_id } = require("../controllers/propertyController.js");
+const {
+  create_property,
+  get_property_by_id,
+  delete_property_id
+} = require("../controllers/propertyController.js");
 
 const router = express.Router();
 
+// create a new property
 router.post("/create", authMiddleware, create_property);
 
-router.get("/", authMiddleware, get_property_by_id)
+// get a property frim the database when you pass the id as query parameter
+router.get("/", authMiddleware, get_property_by_id);
 
+// delete a property from the server
+router.delete("/delete", authMiddleware, delete_property_id);
 
 module.exports = router;
