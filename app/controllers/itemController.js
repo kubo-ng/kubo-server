4abc8be2001c735d571cc39b195d10e2c307c07f
emekarr <aceanyanwu02@gmail.com>
2021-03-18 00:14:57 +0100

    set the server up to be able to delete items

--- app/controllers/itemController.js
+++ app/controllers/itemController.js
@@ -1,34 +1,54 @@
-const Item = require("../models/item.js")
+const Item = require("../models/item.js");
 
 const create_item = async (req, res) => {
-    const item_info = req.body.item_info;
-    item_info.owner = req.body.user._id;
-  
-    try {
-      const item = new Item(item_info);
-      await item.save();
-      res.send({ itemCreated: true, id: item._id });
-    } catch (e) {
-      res.send({
-        itemCreated: false,
-        error: e.message,
-      });
-    }
-}
+  const item_info = req.body.item_info;
+  item_info.owner = req.body.user._id;
+
+  try {
+    const item = new Item(item_info);
+    await item.save();
+    res.send({ itemCreated: true, id: item._id });
+  } catch (e) {
+    res.send({
+      itemCreated: false,
+      error: e.message,
+    });
+  }
+};
 
 const get_item_by_id = async (req, res) => {
-    const item_id = req.query.id;
-  
-    try {
-      const item = await Item.findById(item_id);
-      if (!item) throw new Error("Could not find an item with the id provided.");
-      res.send({ item });
-    } catch (e) {
-      res.send({ item: null, error: e.message });
-    }
-  };
+  const item_id = req.query.id;
+
+  try {
+    const item = await Item.findById(item_id);
+    if (!item) throw new Error("Could not find an item with the id provided.");
+    res.send({ item });
+  } catch (e) {
+    res.send({ item: null, error: e.message });
+  }
+};
+
+const delete_item_id = async (req, res) => {
+  const item_id = req.query.id;
+  let is_item_deleted;
+
+  try {
+    const item =  await Item.findByIdAndDelete(item_id);
+    if (!item) throw new Error("Could not delete item with id provided.");
+
+    is_item_deleted = true;
+    res.send({ is_item_deleted });
+  } catch (e) {
+    is_item_deleted = false;
+    res.send({
+      is_item_deleted,
+      error: e.message,
+    });
+  }
+};
 
 module.exports = {
-    create_item,
-    get_item_by_id
-}
+  create_item,
+  get_item_by_id,
+  delete_item_id
+};
--- app/routes/itemRoute.js
+++ app/routes/itemRoute.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const authMiddleware = require("../middlewares/authMiddleware.js");
-const { create_item, get_item_by_id } = require("../controllers/itemController.js");
+const {
+  create_item,
+  get_item_by_id,
+  delete_item_id,
+} = require("../controllers/itemController.js");
 const router = express.Router();
 
 // create an item in the database
 router.post("/create", authMiddleware, create_item);
 // get an item when an id is provided in the query string
 router.get("/", authMiddleware, get_item_by_id);
+router.delete("/delete", authMiddleware, delete_item_id);
 
 module.exports = router;
